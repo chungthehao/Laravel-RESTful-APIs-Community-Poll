a1ef47b4e1831f4815ffef12f096245bbadb31ed
Chung Thế Hào <chungthehao@gmail.com>
2019-09-16 23:20:24 +0700

    HttpHeaders middleware, how to pass data to middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,8 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            Logging::class
+            Logging::class,
+            'httpHeaders:Come work with us!!!'
         ],
     ];
 
@@ -62,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'httpHeaders' => \App\Http\Middleware\HttpHeaders::class,
     ];
 
     /**
